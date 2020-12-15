89b37fe44f2d002079b964cf11a52f45e75d5a5c
Assaf Morami <assaf.morami@gmail.com>
2020-12-15 17:09:25 +0200

    info modal cosmetics

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -31,14 +31,15 @@ export const Info = ({ title }: { title: string }) => (
 
       <p>
         You can also do the reverse transfer, i.e., redeem your tokens back to
-        your ethereum account. This will cost you SCRT gas, and a fee of about 500K gas,
-        in the denomination that is being withdrawn to cover the multisig transaction costs.
+        your ethereum account. This will cost you SCRT gas, and a fee of about
+        500K gas, in the denomination that is being withdrawn to cover the
+        multisig transaction costs.
       </p>
       <p>
         <b>
           Report any issues in the <strong>#🌉bridge-support</strong> channel on
           the{' '}
-          <a href="http://chat.scrt.network/" target="_blank">
+          <a href="https://chat.scrt.network/" target="_blank">
             Secret Network Discord server
           </a>{' '}
           with one or more of the following information:
@@ -46,7 +47,7 @@ export const Info = ({ title }: { title: string }) => (
         <ul>
           <li>
             1) Transaction id, e.g.,{' '}
-            <span>7fa14f19-219f8220-1f209e61-8911e539</span> in . Every bridge
+            <span>7fa14f19-219f8220-1f209e61-8911e539</span>. Every bridge
             operation is associated with a unique transaction id, which is
             available in your webpage URL. If you didn't store the transaction
             id, it is okay, follow 2) or 3)
@@ -60,10 +61,10 @@ export const Info = ({ title }: { title: string }) => (
       <p>
         <b>Disclaimer</b>
         <br />
-          Use at your own risk. We take no responsibilities or any liability for any claim, damages
-        or other liabilities that may arise from use of this software.
+        Use at your own risk. We take no responsibilities or any liability for
+        any claim, damages or other liabilities that may arise from use of this
+        software.
       </p>
     </div>
   </Box>
 );
-
