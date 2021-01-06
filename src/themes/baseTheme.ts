1b97a7764420a09bf64a63eacc3dd3e10f290c63
Itzik Grossman <itzik@keytango.io>
2021-01-06 17:41:33 +0200

    Check out my style

--- src/themes/baseTheme.ts
+++ src/themes/baseTheme.ts
@@ -79,6 +79,17 @@ const palette: IPalette = {
   BlackTxt: '#212D5E',
 };
 
+//body {
+//   min-height: 100vh;
+//   background-position: 0 -30vh;
+//   background-repeat: no-repeat;
+//   background-image: ${({ theme }) =>
+//     `radial-gradient(50% 50% at 50% 50%, ${transparentize(0.9, theme.primary1)} 0%, ${transparentize(
+//       1,
+//       theme.bg1
+//     )} 100%)`};
+// }
+
 export const baseTheme: any = {
   // storybook theming
   colorPrimary: 'black',
