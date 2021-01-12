a1cc30fa3061fd6760dec9314bfffb5977311696
Assaf Morami <assaf.morami@gmail.com>
2021-01-13 00:03:40 +0200

    ClaimBox update when props.available gets changed

--- src/components/Earn/EarnRow/ClaimBox.tsx
+++ src/components/Earn/EarnRow/ClaimBox.tsx
@@ -1,30 +1,45 @@
 import cn from 'classnames';
 import * as styles from './styles.styl';
 import ClaimButton from './ClaimButton';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ScrtTokenBalance from '../ScrtTokenBalance';
 import { UserStoreEx } from '../../../stores/UserStore';
 
-const ClaimBox = (props: {rewardsContract: string, decimals?: string | number, userStore: UserStoreEx, available: string}) => {
+const ClaimBox = (props: {
+  rewardsContract: string;
+  decimals?: string | number;
+  userStore: UserStoreEx;
+  available: string;
+}) => {
+  const [available, setAvailable] = useState<string>(props.available);
+  useEffect(() => {
+    setAvailable(props.available);
+  }, [props.available]);
+
   return (
     <div className={cn(styles.claimBox)}>
       <div>
         <div className={cn(styles.items)}>
           <ScrtTokenBalance
-           subtitle={"Available Rewards"}
-           tokenAddress={props.rewardsContract}
-           decimals={props.decimals || 0}
-           userStore={props.userStore}
-           currency={"sSCRT"} selected={false} value={props.available}/>
+            subtitle={'Available Rewards'}
+            tokenAddress={props.rewardsContract}
+            decimals={props.decimals || 0}
+            userStore={props.userStore}
+            currency={'sSCRT'}
+            selected={false}
+            value={available}
+          />
           {/*<SoftTitleValue title={`${} sSCRT`} subTitle={} />*/}
         </div>
-        {<ClaimButton
-          secretjs={props.userStore.secretjs}
-          contract={props.rewardsContract}
-        />}
+        {
+          <ClaimButton
+            secretjs={props.userStore.secretjs}
+            contract={props.rewardsContract}
+          />
+        }
       </div>
     </div>
   );
-}
+};
 
 export default ClaimBox;
