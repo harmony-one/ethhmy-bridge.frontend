3fd81cb06daf7ccb2a42b94f183d5cd4ce217ace
Assaf Morami <assaf.morami@gmail.com>
2021-01-12 22:35:25 +0200

    fix /earn isn't updated properly by websocket balance updage

--- src/components/Earn/ScrtTokenBalance/index.tsx
+++ src/components/Earn/ScrtTokenBalance/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Loader from 'react-loader-spinner';
 import SoftTitleValue from '../SoftTitleValue';
 import UnlockToken from '../EarnRow/UnlockToken';
@@ -9,54 +9,75 @@ import cn from 'classnames';
 import { divDecimals, unlockToken } from '../../../utils';
 
 const formatNumber = (amount: string, minimumFactions: number) => {
-  return new Intl.NumberFormat('en', { maximumFractionDigits: 6, minimumFractionDigits: minimumFactions })
-    .format(Number(amount))
-}
+  return new Intl.NumberFormat('en', {
+    maximumFractionDigits: 6,
+    minimumFractionDigits: minimumFactions,
+  }).format(Number(amount));
+};
 
-const ScrtTokenBalance = (props: {value: string, decimals: string | number, currency: string, subtitle?: string,
-  userStore: UserStoreEx, tokenAddress: string, selected: boolean, minimumFactions? : number}) => {
+const ScrtTokenBalance = (props: {
+  value: string;
+  decimals: string | number;
+  currency: string;
+  subtitle?: string;
+  userStore: UserStoreEx;
+  tokenAddress: string;
+  selected: boolean;
+  minimumFactions?: number;
+}) => {
+  const [value, setValue] = useState<string>(props.value);
+  useEffect(() => {
+    setValue(props.value);
+  }, [props.value]);
 
-  const text = props.subtitle ? props.subtitle : "Available Balance"
+  const text = props.subtitle ? props.subtitle : 'Available Balance';
 
-  const minimumFactions = props.minimumFactions !== undefined ? props.minimumFactions : 3;
+  const minimumFactions =
+    props.minimumFactions !== undefined ? props.minimumFactions : 3;
 
-  if (!props.value) {
-    return (<SoftTitleValue
-        title={<Loader type="ThreeDots" color="#00BFFF" height="1em" width="1em" />}
+  if (!value) {
+    return (
+      <SoftTitleValue
+        title={
+          <Loader type="ThreeDots" color="#00BFFF" height="1em" width="1em" />
+        }
         subTitle={text}
       />
     );
-  } else if (props.value === unlockToken) {
+  } else if (value === unlockToken) {
     return (
-      <div style={{margin: 0, display: "flex", flex: 1}}>
+      <div style={{ margin: 0, display: 'flex', flex: 1 }}>
         <UnlockToken
           userStore={props.userStore}
           tokenAddress={props.tokenAddress}
           selected={props.selected}
         />
         <SoftTitleValue
-          title={"Unlock Token"}
-          subTitle={"Rewards unavailable"}
+          title={'Unlock Token'}
+          subTitle={'Rewards unavailable'}
         />
       </div>
-      );
+    );
   } else {
     return (
       <SoftTitleValue
         title={
           <div className={cn(styles.assetRow)}>
             <NumberOdometer
-              number={`${formatNumber(divDecimals(props.value.replace(/,/g, ''), props.decimals), minimumFactions)}`}
+              number={`${formatNumber(
+                divDecimals(value.replace(/,/g, ''), props.decimals),
+                minimumFactions,
+              )}`}
             />
-            <div style={{ marginLeft: "5px", paddingTop: "4px" }}>{props.currency}</div>
+            <div style={{ marginLeft: '5px', paddingTop: '4px' }}>
+              {props.currency}
+            </div>
           </div>
         }
         subTitle={text}
       />
     );
   }
-
-
 };
 
 export default ScrtTokenBalance;
