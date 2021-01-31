149e863aa185b26a4c6b90696b43e727c1d77884
Assaf Morami <assaf.morami@gmail.com>
2021-01-31 20:01:38 +0200

    ws can now use only wasm.contract_address because we added nop logs on every contract handle request

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -479,21 +479,15 @@ export class SwapRouter extends React.Component<
         } else {
           // Any tx on the token's contract
           const tokenAddress = tokens[tokenSymbol].address;
-          const tokenQueries = [
-            `message.contract_address='${tokenAddress}'`,
-            `wasm.contract_address='${tokenAddress}'`,
-          ];
 
-          for (const query of tokenQueries) {
-            this.ws.send(
-              JSON.stringify({
-                jsonrpc: '2.0',
-                id: tokenSymbol, // jsonrpc id
-                method: 'subscribe',
-                params: { query },
-              }),
-            );
-          }
+          this.ws.send(
+            JSON.stringify({
+              jsonrpc: '2.0',
+              id: tokenSymbol, // jsonrpc id
+              method: 'subscribe',
+              params: { query: `wasm.contract_address='${tokenAddress}'` },
+            }),
+          );
         }
       }
 
@@ -518,9 +512,7 @@ export class SwapRouter extends React.Component<
         const lpTokenAddress = pairFromSymbol[pairSymbol].liquidity_token;
 
         const pairQueries = [
-          `message.contract_address='${pairAddress}'`,
           `wasm.contract_address='${pairAddress}'`,
-          `message.contract_address='${lpTokenAddress}'`,
           `wasm.contract_address='${lpTokenAddress}'`,
         ];
 
