53ddbec36fa4cfdfbcd8e0e856be10c91629b4ee
Assaf Morami <assaf.morami@gmail.com>
2021-02-10 19:13:49 +0200

    TokenSelector exitIcon

--- src/pages/Swap/TokenSelector.tsx
+++ src/pages/Swap/TokenSelector.tsx
@@ -9,6 +9,23 @@ import { SigningCosmWasmClient } from 'secretjs';
 import LocalStorageTokens from '../../blockchain-bridge/scrt/CustomTokens';
 import Loader from 'react-loader-spinner';
 
+const exitIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <line x1="18" y1="6" x2="6" y2="18"></line>
+    <line x1="6" y1="6" x2="18" y2="18"></line>
+  </svg>
+);
+
 export const TokenSelector = (props: {
   secretjs: SigningCosmWasmClient;
   tokens: TokenDisplay[];
@@ -54,7 +71,14 @@ export const TokenSelector = (props: {
       trigger={<TokenSelectorButton token={props.token} />}
       style={{ width: '700px' }}
     >
-      <Modal.Header>Select Token</Modal.Header>
+      <Modal.Header>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <span>Select a token</span>
+          <span style={{ cursor: 'pointer' }} onClick={() => setOpen(false)}>
+            {exitIcon}
+          </span>
+        </div>
+      </Modal.Header>
       <Modal.Content>
         {props.tokens.length > 0 ? (
           props.tokens.map(t => {
