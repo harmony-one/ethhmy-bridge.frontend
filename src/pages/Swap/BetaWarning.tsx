3342879c7132ac1d0e56fa2e68e1f1deb851f7c8
Assaf Morami <assaf.morami@gmail.com>
2021-02-17 20:46:51 +0200

    early birds message

--- src/pages/Swap/BetaWarning.tsx
+++ src/pages/Swap/BetaWarning.tsx
@@ -4,7 +4,19 @@ import { Message } from 'semantic-ui-react';
 
 export const BetaWarning = ({ secretjs }: { secretjs: SigningCosmWasmClient }) => {
   if (process.env.ENV === 'MAINNET') {
-    return null;
+    return (
+      <Message warning>
+        <Message.Header>Hello early birds! 👋</Message.Header>
+        <p>
+          <strong>Converting SCRT to sSCRT:</strong> Use{' '}
+          <a href="https://wallet.keplr.app/#/secret-2/secret-secret" target="_blank">
+            this tool
+          </a>{' '}
+          by Keplr
+        </p>
+        <p>Enjoy your front-running resistant swaps!</p>
+      </Message>
+    );
   }
 
   return (
