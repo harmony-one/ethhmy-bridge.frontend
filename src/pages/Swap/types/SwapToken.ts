62214c22fdec93a99f2b459b6043328b37029560
Assaf Morami <assaf.morami@gmail.com>
2021-02-21 22:38:47 +0200

    prepend s to token symbols

--- src/pages/Swap/types/SwapToken.ts
+++ src/pages/Swap/types/SwapToken.ts
@@ -34,9 +34,18 @@ export const TokenMapfromITokenInfo = (tokens: ITokenInfo[]): SwapTokenMap => {
       : validateBech32Address(t.src_address)
       ? t.src_address
       : '';
-    let swapToken: SwapToken = {
+    let symbol;
+    if (t.display_props.symbol === 'SCRT') {
+      symbol = 'SCRT';
+    } else if (t.display_props.symbol.toLowerCase() === 'sscrt') {
+      symbol = 'sSCRT';
+    } else {
+      symbol = 's' + t.display_props.symbol;
+    }
+
+    const swapToken: SwapToken = {
       identifier: secretAddress,
-      symbol: t.display_props.symbol,
+      symbol: symbol,
       logo: t.display_props.image,
       decimals: Number(t.decimals),
       name: t.name,
