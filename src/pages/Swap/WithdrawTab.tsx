b109439634d9c1358c5d56995e98f4ea5c676bbe
Assaf Morami <assaf.morami@gmail.com>
2021-02-25 18:46:19 +0200

    WithdrawTab sort sSCRT pairs first

--- src/pages/Swap/WithdrawTab.tsx
+++ src/pages/Swap/WithdrawTab.tsx
@@ -57,23 +57,46 @@ export class WithdrawTab extends React.Component<{
     return (
       <Container className={cn(styles.swapContainerStyle)}>
         <TabsHeader />
-        {pairs.map(p => {
-          return (
-            <span key={p.lpTokenSymbol()}>
-              <WithdrawLiquidityPanel
-                lpTokenSymbol={p.lpTokenSymbol()}
-                tokens={this.props.tokens}
-                selectedPair={p}
-                balances={this.props.balances}
-                secretjs={this.props.secretjs}
-                notify={this.props.notify}
-                getBalance={this.props.updateToken}
-                onCloseTab={this.props.onCloseTab}
-              />
-              <div style={{ minHeight: '1em' }} />
-            </span>
-          );
-        })}
+        {pairs
+          .sort((p1, p2) => {
+            const [p1SymbolA, p1SymbolB] = [p1.asset_infos[0].symbol, p1.asset_infos[1].symbol];
+            let p1Symbol = `${p1SymbolA}-${p1SymbolB}`;
+            if (p1SymbolB === 'sSCRT') {
+              p1Symbol = `${p1SymbolB}-${p1SymbolA}`;
+            }
+
+            const [p2SymbolA, p2SymbolB] = [p2.asset_infos[0].symbol, p2.asset_infos[1].symbol];
+            let p2Symbol = `${p2SymbolA}-${p2SymbolB}`;
+            if (p2SymbolB === 'sSCRT') {
+              p2Symbol = `${p2SymbolB}-${p2SymbolA}`;
+            }
+
+            if (p1Symbol.startsWith('sSCRT-') && !p2Symbol.startsWith('sSCRT-')) {
+              return -1;
+            }
+            if (p2Symbol.startsWith('sSCRT-') && !p1Symbol.startsWith('sSCRT-')) {
+              return 1;
+            }
+
+            return p1Symbol.localeCompare(p2Symbol);
+          })
+          .map(p => {
+            return (
+              <span key={p.lpTokenSymbol()}>
+                <WithdrawLiquidityPanel
+                  lpTokenSymbol={p.lpTokenSymbol()}
+                  tokens={this.props.tokens}
+                  selectedPair={p}
+                  balances={this.props.balances}
+                  secretjs={this.props.secretjs}
+                  notify={this.props.notify}
+                  getBalance={this.props.updateToken}
+                  onCloseTab={this.props.onCloseTab}
+                />
+                <div style={{ minHeight: '1em' }} />
+              </span>
+            );
+          })}
       </Container>
     );
   }
