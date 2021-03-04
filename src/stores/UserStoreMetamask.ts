869b40cb99f422b056d07481a6f6af1eeec988bc
Clipart <lourencobrutocosta@gmail.com>
2021-03-04 12:02:52 +0000

    Fix old typescript error

--- src/stores/UserStoreMetamask.ts
+++ src/stores/UserStoreMetamask.ts
@@ -124,7 +124,7 @@ export class UserStoreMetamask extends StoreConstructor {
                   },
                 ],
               },
-              err => (err ? reject(err) : accept()),
+              err => (err ? reject(err) : accept("success")),
             ),
           );
         }
