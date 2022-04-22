c47950a12df5c9908b3334af999ea9060a329113
ahiipsa <ahiipsa@gmail.com>
2022-04-23 01:52:40 +0400

    Draft: Fixed NFT Tokens

--- src/pages/EthBridge/components/StepBASE/components/TokenControl/TokenControl.tsx
+++ src/pages/EthBridge/components/StepBASE/components/TokenControl/TokenControl.tsx
@@ -8,6 +8,7 @@ import { ModalIds, ModalRegister } from '../../../../../../modals';
 import { TokenChooseModal } from '../../../TokenChooseModal/TokenChooseModal';
 import { observer } from 'mobx-react';
 import { TOKEN } from '../../../../../../stores/interfaces';
+import { isMultiNFT, isNFT } from '../../../../../../stores/Exchange/helpers';
 
 interface Props {}
 
@@ -18,7 +19,9 @@ const customTokens = [TOKEN.ERC721, TOKEN.HRC721, TOKEN.HRC1155, TOKEN.ERC1155];
 export const TokenControl: React.FC<Props> = observer(() => {
   const { routing, exchange } = useStores();
 
-  const isH = [...selectAllow, ...customTokens].includes(exchange.token);
+  const isSelectable = [...selectAllow, ...customTokens].includes(
+    exchange.token,
+  );
   const handleChangeToken = useCallback(() => {
     if (selectAllow.includes(exchange.token)) {
       routing.goToModal(ModalIds.BRIDGE_TOKEN_CHOOSE);
@@ -32,18 +35,28 @@ export const TokenControl: React.FC<Props> = observer(() => {
     return;
   }, [routing, exchange, exchange.token]);
 
+  const tokenName = useMemo(() => {
+    const title =
+      isNFT(exchange.token) || isMultiNFT(exchange.token) ? 'Choose NFT' : '';
+
+    if (exchange.tokenInfo) {
+      return exchange.tokenInfo.symbol || exchange.tokenInfo.label || title;
+    }
+
+    return title;
+  }, [exchange.token, exchange.tokenInfo]);
+
   return (
     <BridgeControl
       title="Choose Token"
       gap="8px"
       centerContent={
-        <Button disabled={!isH} onClick={handleChangeToken}>
+        <Button disabled={!isSelectable} onClick={handleChangeToken}>
           <Box direction="row" gap="8px">
             <Text size="large" color="NWhite">
-              {exchange.tokenInfo &&
-                (exchange.tokenInfo.symbol || exchange.tokenInfo.label)}
+              {tokenName}
             </Text>
-            {isH && <Icon size="10px" glyph="ArrowDownFilled" />}
+            {isSelectable && <Icon size="10px" glyph="ArrowDownFilled" />}
           </Box>
         </Button>
       }
--- src/pages/EthBridge/components/StepBASE/components/TokenRow/TokenRow.tsx
+++ src/pages/EthBridge/components/StepBASE/components/TokenRow/TokenRow.tsx
@@ -8,6 +8,7 @@ import { useStores } from '../../../../../../stores';
 import { BridgeControl } from '../../../BridgeControl/BridgeControl';
 import { TOKEN } from '../../../../../../stores/interfaces';
 import { truncateAddressString } from '../../../../../../utils';
+import { isNFT } from '../../../../../../stores/Exchange/helpers';
 
 interface Props {}
 
@@ -27,14 +28,18 @@ export const TokenRow: React.FC<Props> = observer(() => {
         <Box basis="0" flex="grow">
           <TokenControl />
         </Box>
-        <Box alignSelf="center">
-          {exchange.tokenInfo && (
-            <img src={exchange.tokenInfo.image} width="40" />
-          )}
-        </Box>
-        <Box basis="0" flex="grow">
-          <TokenAmount />
-        </Box>
+        {!isNFT(exchange.token) && (
+          <Box alignSelf="center">
+            {exchange.tokenInfo && (
+              <img src={exchange.tokenInfo.image} width="40" />
+            )}
+          </Box>
+        )}
+        {!isNFT(exchange.token) && (
+          <Box basis="0" flex="grow">
+            <TokenAmount />
+          </Box>
+        )}
       </Box>
       {displayTokenAddress && (
         <Box justify="center" align="center" pad={{ top: 'xsmall' }}>
